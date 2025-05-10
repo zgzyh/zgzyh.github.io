32ef2e80abe248ae2961ffd4b34283e7af73a716
我願一直向著陽光 <103393141+zgzyh@users.noreply.github.com>
2025-05-10 11:08:16 +0800

    更新 cc.bat

--- cc.bat
+++ cc.bat
@@ -1,1 +1,10 @@
-start jre\java11\bin\javaw.exe -jar lizzie.jar --ruleset custom --disable-points random:5
+@echo off
+chcp 65001 > nul
+
+REM 先运行Python脚本生成禁点并启动引擎
+python random_ban.py
+
+REM 原启动命令（可选，如果需同时启动Lizzie界面）
+REM start jre\java11\bin\javaw.exe -jar lizzie.jar
+
+pause
